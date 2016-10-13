3ab5289bcc37f3a80f1cffab96a981e7211d0399
Daniel Pliego <danielpliego@gmail.com>
2016-10-13 08:36:05 -0500

    felipe espejismo page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".various").fancybox({
+		maxWidth	: 1920,
+		maxHeight	: 900,
+		fitToView	: false,
+		width		: '85%',
+		height		: '100%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+});
